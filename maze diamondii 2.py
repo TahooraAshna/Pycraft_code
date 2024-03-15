5c32b5e460a67b503ce41095f62ab04462581f2c
TahooraAshna <159803698+TahooraAshna@users.noreply.github.com>
2024-03-16 00:23:43 +0330

    Add files via upload

---
+++ maze diamondii 2.py
@@ -0,0 +1,110 @@
+from mcpi_e.minecraft import *
+from mcpi_e.block import *
+import collections
+collections.Iterable=collections.abc.Iterable
+import collections.abc
+from _collections_abc import Iterable
+from time import *
+
+serverAddress = "Pycraft.yasan.ac"
+pythonPort = 29035
+PlayerName = "t_vahedi"
+
+x , z, y = mc.player.getTilePos()
+
+dir = "e"
+road_points = []
+  
+while True :
+    mc.setBlock(x,z,y,AIR )
+
+    if dir == "e" :
+        if mc.getBlock(x,z+1,y) == 0 :
+            z += 1   
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "s"
+        elif mc.getBlock(x+1 ,z ,y) == 0 :
+            x += 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "e"
+        elif mc.getBlock(x ,z-1 ,y) == 0 :
+            z -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "n"
+        elif mc.getBlock(x-1 ,z ,y) == 0 :
+            x -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "w"
+
+    elif dir == "s" :
+        if mc.getBlock(x-1 ,z ,y) == 0 :
+            x -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "w"  
+        elif mc.getBlock(x,z+1,y) == 0 :
+            z += 1   
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "s"
+        elif mc.getBlock(x+1 ,z ,y) == 0 :
+            x += 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "e"
+        elif mc.getBlock(x ,z-1 ,y) == 0 :
+            z -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "n"
+            
+    elif dir == "w" :
+        if mc.getBlock(x ,z-1 ,y) == 0 :
+            z -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "n"
+        elif mc.getBlock(x-1 ,z ,y) == 0 :
+            x -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "w"  
+        elif mc.getBlock(x,z+1,y) == 0 :
+            z += 1   
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "s"
+        elif mc.getBlock(x+1 ,z ,y) == 0 :
+            x += 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "e"
+
+    elif dir == "n" :
+        if mc.getBlock(x+1 ,z ,y) == 0 :
+            x += 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "e" 
+        elif mc.getBlock(x ,z-1 ,y) == 0 :
+            z -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "n"
+        elif mc.getBlock(x-1 ,z ,y) == 0 :
+            x -= 1
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "w"  
+        elif mc.getBlock(x,z+1,y) == 0 :
+            z += 1   
+            mc.setBlock(x,z,y,GLOWSTONE_BLOCK)
+            dir = "s"
+
+    #mc.setBlock(x,y,z ,GLOWSTONE_BLOCK)
+    point =[x,y,z]
+    if point in road_points :
+        i = road_points.index(point)
+        del road_points[i:]
+    road_points.append(point)
+
+    if mc.getBlock(x,y-1,z) == 57 :
+        mc.PostToChat ("finish") 
+        break
+
+    sleep(0.4)
+
+for p in road_points :
+    mc.setBlock(x,y,z, AIR)
+    x ,y ,z = p
+    mc.setBlock(x ,y ,z , GLOWSTONE_BLOCK)
+    sleep(0.2)
